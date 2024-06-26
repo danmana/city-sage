e0499a053f8c63f8b1463305404cb96482e8e8ec
Dan Manastireanu <498419+danmana@users.noreply.github.com>
2024-06-26 19:16:28 +0300

    chore: og meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,19 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "City Sage",
   description: "self-guided walking tours made for you by ai",
+  openGraph: {
+    type: "website",
+    url: "https://citysage.me",
+    title: "City Sage",
+    description: "self-guided walking tours made for you by ai",
+    images: "https://www.citysage.me/city-sage.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@danmana",
+    creator: "@danmana",
+    images: "https://www.citysage.me/city-sage.png",
+  },
 };
 
 export default function RootLayout({
