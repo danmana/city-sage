ee78f489687aac9fb5d28192b003fdda151ae61e
Dan Manastireanu <498419+danmana@users.noreply.github.com>
2024-06-26 11:12:51 +0300

    chore: less padding

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { ComingSoon } from "../components/ComingSoon";
 
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between py-16 px-8 md:py-24 md:px-16">
+  return <main className="flex min-h-screen flex-col items-center justify-between">
     <ComingSoon />
   </main>;
 }
